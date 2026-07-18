1439f3097949f821c0fae2f417ecb9a01121ebb3
shaheer41-source <shaheer41@gmail.com>
2026-07-18 14:19:03 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,38 @@
 
+import { auth } from "./firebase.js";
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        alert("Login Successful!");
+
+        window.location.href = "dashboard.html";
+
+    }
+
+    catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
